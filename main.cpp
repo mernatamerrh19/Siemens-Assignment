5285aff7fc2d1308d8694976cdde326e09fdd660
Merna <merna.youssef01@eng-st.cu.edu.eg>
2023-11-03 15:48:46 +0200

    first-branch-adding

--- main.cpp
+++ main.cpp
@@ -11,8 +11,16 @@ int getVectorSum(vector<int>v){
     return sum;
 }
 
+int getAverage(vector<int>v){
+    int sum = getVectorSum(v);
+    int num = v.size();
+    int avg = sum/num;
+    return avg;
+}
+
 int main() {
     vector<int> v = {1,2,3,4,5,6,7,8,9,10};
     cout<< getVectorSum(v)<< endl;
+    cout<< getAverage(v)<<endl;
     return 0;
 }
